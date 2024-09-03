fddcd9d25e82deff673dcb1f587c2ef4e48f0fbb
xploreweb <vaiko.edur@tptlive.ee>
2024-09-03 13:54:34 +0300

    git init

---
+++ unscramble.php
@@ -0,0 +1,7 @@
+// Read in the wordlist to an array
+// Read in the scrambled words.
+// Give the scrambled words a value
+// Give the wordlist words a value
+// Check the numbers against each other
+// Print the word if you have a match
+
